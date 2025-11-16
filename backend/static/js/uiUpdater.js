fc7a83b3d50c563b260645e851bab24ae9f87f6b
liquorburn <droidseven@libero.it>
2025-11-16 23:09:18 +0100

    eliminato file inutilizzato

--- backend/static/js/uiUpdater.js
+++
@@ -1,3 +0,0 @@
-// This file is no longer needed as all UI updates are handled by server-side rendering with Jinja2.
-// The logic has been moved to the backend templates.
-// This file can be safely deleted.
